a2eb7453c99c09df8e5e59cf43f6223b58e16826
Jun <237489473@qq.com>
2018-08-15 11:36:30 +0800

    更新

---
+++ appupdateutils/src/main/java/com/luxiaochun/appupdateutils/App.java
@@ -0,0 +1,13 @@
+package com.luxiaochun.appupdateutils;
+
+import android.app.Application;
+
+import com.lzy.okgo.OkGo;
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        OkGo.getInstance().init(this);
+    }
+}
